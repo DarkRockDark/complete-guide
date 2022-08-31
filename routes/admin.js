e44c9503265a0d8d188bdfd2027992b31ebcd7ea
Doug Rogers <drogers@netroadshow.com>
2022-08-31 10:43:05 -0400

    templating

--- routes/admin.js
+++ routes/admin.js
@@ -9,7 +9,11 @@ const products = [];
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-    res.render('add-product', {pageTitle: 'Add Products', path: '/admin/add-product'});
+    res.render('add-product', {
+        pageTitle: 'Add Product', 
+        path: '/admin/add-product',
+        activeAddProduct: true
+    });
 });
 
 // /admin/add-product => POST
