737bebfb00a3afdb8e7bd79b5e3bf0686ee16eb5
Doug Rogers <drogers@netroadshow.com>
2022-08-31 11:42:23 -0400

    mvc start

--- app.js
+++ app.js
@@ -2,29 +2,23 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars');
 
 const app = express();
 
-// app.engine('hbs', expressHbs({
-//     layoutsDir: 'views/layouts/', 
-//     defaultLayout: 'main-layout', 
-//     extname: 'hbs'
-// }));
-// app.set('view engine', 'hbs');
 app.set('view engine', 'ejs');
-// app.set('view engine', 'pug');
 app.set('views', 'views');
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-const fourOfour = require('./routes/pageNotFound');
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin',adminData.routes);
+app.use('/admin', adminData.routes);
 app.use(shopRoutes);
-app.use(fourOfour);
+
+app.use((req, res, next) => {
+  res.status(404).render('404', { pageTitle: 'Page Not Found' });
+});
 
 app.listen(3000);
--- routes/shop.js
+++ routes/shop.js
@@ -8,14 +8,15 @@ const adminData = require('./admin');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    const products = adminData.products;
-    res.render('shop', { 
-        prods: products, 
-        pageTitle: 'Shop', 
-        path: '/', 
-        hasProducts: products.length > 0 ,
-        activeShop: true
-    });
+  const products = adminData.products;
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true
+  });
 });
 
 module.exports = router;
