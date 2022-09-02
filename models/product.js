d5a5b71cc40a208418389a8a67375ee47744471e
Doug Rogers <drogers@netroadshow.com>
2022-09-02 14:02:38 -0400

    begin routes

--- models/product.js
+++ models/product.js
@@ -1,44 +1,40 @@
-const { json } = require('body-parser');
 const fs = require('fs');
 const path = require('path');
-const { compile } = require('pug');
+
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
+
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
 
 module.exports = class Product {
-    constructor(t) {
-        this.title = t;
-    }
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
+  }
 
-    save () {
-        // products.push(this);
-        const p = path.join(
-            path.dirname(process.mainModule.filename), 
-            'data', 
-            'products.json'
-            );
-        fs.readFile(p, (err, fileContent) => {
-            let products = [];
-            if (!err) {
-                products = JSON.parse(fileContent);
-            }
-            products.push(this);
-            fs.writeFile(p, JSON.stringify(products), (err) => {
-                console.log(err);
-            });
-        });
-    }
+  save() {
+    getProductsFromFile(products => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
+    });
+  }
 
-    static fetchAll(cb) {
-        const p = path.join(
-            path.dirname(process.mainModule.filename), 
-            'data', 
-            'products.json'
-            );
-        fs.readFile(p, (err, fileContent) => {
-            if (err) {
-                console.log(err);
-                cb([]);
-            }
-            cb(JSON.parse(fileContent));
-        });
-    }
-}
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
+};
