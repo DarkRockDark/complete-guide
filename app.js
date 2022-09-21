980d6cad89f98e0f1aab3a2574eb0bf868060635
Doug Rogers <drogers@netroadshow.com>
2022-09-21 09:18:21 -0400

    cookie restart

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -29,26 +30,31 @@ app.use((req, res, next) => {
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use(errorController.get404);
 
 mongoose
   .connect(
-    'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0-ntrwp.mongodb.net/shop?retryWrites=true'
+    'mongodb+srv://drogers:brass-flute@cluster0.mvmsyv7.mongodb.net/test'
   )
   .then(result => {
-    User.findOne().then(user => {
-      if (!user) {
-        const user = new User({
-          name: 'Max',
-          email: 'max@test.com',
-          cart: {
-            items: []
-          }
-        });
-        user.save();
-      }
-    });
+    User.findOne()
+      .then(user => {
+        if (!user) {
+          const user = new User({
+            name: 'doug',
+            email: 'test@test.com',
+            cart: {
+              items: []
+            }
+          });
+          user.save();
+        }
+      })
+      .catch(
+        err => console.log('findOne ' + err)
+      );
     app.listen(3000);
   })
   .catch(err => {
